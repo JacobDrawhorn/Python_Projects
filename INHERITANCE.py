0ca3afc8a2105587486e4f6d96ac87f5ab86b215
JacobDrawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2021-09-10 15:46:25 -0700

    Update INHERITANCE.py

--- INHERITANCE.py
+++ INHERITANCE.py
@@ -1,13 +1,17 @@
+
+# parent class
 class User:
     name = 'No Name Provided'
     email = ' '
     password = '1234abcd'
     account_number = 0
 
+# child class
 class Employee(User):
     base_pay = 11.00
     department = 'General'
 
+# child class
 class Customer(User):
     mailing_address = ' '
     mailing_list = True
